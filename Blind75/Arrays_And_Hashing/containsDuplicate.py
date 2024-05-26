fd5f9c6b067e8a274b5ef7255df1b3dd3c6a7265
pairofAces <karanchauhan16@gmail.com>
2024-05-26 17:08:05 -0400

    inital commit, created 2 folders

---
+++ Blind75/Arrays_And_Hashing/containsDuplicate.py
@@ -0,0 +1,7 @@
+# Duplicate Integer
+
+# Given an integer array, nums, return true if any value appears
+# more than once in the input array, otherwise return false. 
+
+def hasDuplicate(nums: List[int]):
+    
