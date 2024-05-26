28fbd9b4cf515fc4f625be989f934eb5c0b81b33
pairofAces <karanchauhan16@gmail.com>
2024-05-26 17:15:16 -0400

    created solution and test

--- Blind75/Arrays_And_Hashing/containsDuplicate.py
+++ Blind75/Arrays_And_Hashing/containsDuplicate.py
@@ -3,5 +3,18 @@
 # Given an integer array, nums, return true if any value appears
 # more than once in the input array, otherwise return false. 
 
-def hasDuplicate(nums: List[int]):
-    
+def hasDuplicate(nums: list[int]):
+    # using python set 
+    hashset = set()
+
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+
+    return False
+
+nums = [1,2,3,4,4]
+
+test = hasDuplicate(nums)
+print(test)
